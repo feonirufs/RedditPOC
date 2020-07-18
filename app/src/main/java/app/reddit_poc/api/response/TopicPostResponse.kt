feeb83fc58475b4d508eb513b2750ab8716faf8f
thifeonir <thi.alessandro>
2020-07-18 15:25:56 -0300

    Setup json response

---
+++ app/src/main/java/app/reddit_poc/api/response/TopicPostResponse.kt
@@ -0,0 +1,6 @@
+package app.reddit_poc.api.response
+
+data class TopicPostResponse(
+    val kind: String = "",
+    val data: TopicPostDataResponse
+)
---
+++ app/src/main/java/app/reddit_poc/api/response/TopicResponse.kt
@@ -0,0 +1,6 @@
+package app.reddit_poc.api.response
+
+data class TopicResponse(
+    val kind: String = "",
+    val data: TopicDataResponse
+)

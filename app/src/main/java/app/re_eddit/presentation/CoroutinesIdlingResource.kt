e3bac9e4f0627b8808fa0e5429fea518419b9fa5
thifeonir <litjc13@gmail.com>
2020-08-04 02:04:32 -0300

    chore: make idlingResourceName const

--- app/src/main/java/app/re_eddit/presentation/CoroutinesIdlingResource.kt
+++ app/src/main/java/app/re_eddit/presentation/CoroutinesIdlingResource.kt
@@ -4,6 +4,6 @@ import androidx.test.espresso.idling.CountingIdlingResource
 
 
 object CoroutinesIdlingResource {
-    private val idlingResourceName = "COROUTINES_IDLING_RESOURCE"
+    private const val idlingResourceName = "COROUTINES_IDLING_RESOURCE"
     @JvmField val idlingResource = CountingIdlingResource(idlingResourceName)
 }
